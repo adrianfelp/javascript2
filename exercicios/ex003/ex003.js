b43b805faf865adae9d4e0cbe8f5bafcea089b31
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-04-18 15:07:52 -0300

    ex003 let var e const

---
+++ exercicios/ex003/ex003.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function teste() {
+    let nome = 'Adrian'
+    if(true) {
+            console.log('Dentro do if do teste: ' + nome)
+    }
+    console.log('dentro de teste: ' + nome)
+}
+
+teste()
+
+console.log('Fora de teste: ' + nome)
