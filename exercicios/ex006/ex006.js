436b011a4cded3e85e22d7154c0c9561e2c8e4f6
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-04-22 19:03:06 -0300

    ex006 finalizado

---
+++ exercicios/ex006/ex006.js
@@ -0,0 +1,15 @@
+// && -> and e
+// || -> or ou
+// ! -> not não
+
+let n1, n2, n3, n4
+
+n1 = 10
+n2 = 5
+n3 = 15
+n4 = 2
+if(n1 > n2) {
+    console.log(`O número ${n1} é maior que o número ${n2} combinado`)
+} else {
+    console.log(`O número ${n1} é menor que o número ${n2} combinado `)
+}
