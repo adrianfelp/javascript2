d53d699669b8b870d81bcbf1b44bae3ff219a604
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-04-27 14:52:30 -0300

    ex010

---
+++ exercicios/ex010/ex010.js
@@ -0,0 +1,11 @@
+let num = 10
+
+if (num > 10) {
+    console.log(`Numeral maior que 10`)
+} else if (num > 5){
+    console.log(`Numeral está entre 6 e 10`)
+} else {
+    console.log(`Numeral menor ou igual a 5`)
+}
+
+console.log(`Fim do programa`)
