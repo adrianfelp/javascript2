a0d010a7ae2bb0d4cb83673b96006eb16cf221da
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-05-02 17:37:25 -0300

    ex014 fatorial e while

--- exercicios/ex013/ex013.js
+++ exercicios/ex013/ex013.js
@@ -1,12 +1,20 @@
 let num = [10,20,30,40,50]
 
-for(n in num) {
-    console.log(n)
-}
-
-
-/*
+// for normal, as outras são simplificadas
 for (let i = 0; i < num.length; i++) {
     console.log(num[i])
+}
+
+// for of, imprimi os elementos
+/*
+for(n of num) {
+    console.log(n)
+}
+*/
+
+// for in, imprimi a posição dos elementos
+/*
+for (n in num) {
+    console.log(n)
 }
 */
---
+++ exercicios/ex014/ex014.js
@@ -0,0 +1,27 @@
+// fatorial de 5
+// 5! = 5*4*3*2*1 = 120
+
+let n = 5
+let fat = 1
+
+while (n >= 1) {
+    fat *= n
+    n--
+}
+
+console.log(fat)
+
+
+
+
+
+
+
+/*  // loop while
+let n = 8
+
+while (n < 12) {
+    console.log(n)
+    n++
+}
+*/
